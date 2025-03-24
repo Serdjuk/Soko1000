a7448d9e03a808afd92dd1c57f214145f31be056
Виталий <efiop80@gmail.com>
2025-03-24 19:16:15 +0400

    removed the unnecessary

--- other/dumpviewer.bat
+++
@@ -1,4 +0,0 @@
-@echo off   
-set mdir=%cd%
-echo %cd%
-start %cd%\lib\dump-1.0.0.jar
--- sjasmplus/sjasmplus-1.20.3.win/examples/file name test/option zxnext ignore.asm
+++
@@ -1,2 +0,0 @@
-; this file should NOT be assembled by the CI script, it should be on the ignore list
-    some-invalid-instruction    a,123
