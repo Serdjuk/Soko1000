f193d1855d3fee4850eb1898f15377dd0bd37ddd
Nlo-J77 <nlo_j77@rambler.ru>
2025-08-01 23:21:51 +0700

    Update macros.asm

--- code/macros.asm
+++ code/macros.asm
@@ -6,7 +6,7 @@ LOOP:	macro	adr
 LOAD_TAPE:	macro dst, length
 	ld	(Errlod+1),sp
 Errlod:
-ld	de,#0000
+	ld	de,#0000
 	dec	de
 	dec	de
 	ld	(#5C3D),de
@@ -20,7 +20,7 @@ ld	de,#0000
 SAVE_TAPE:	macro dst, length
 	ld	(Errsav+1),sp
 Errsav:
-ld	de,#0000
+	ld	de,#0000
 	dec	de
 	dec	de
 	ld	(#5C3D),de
